4d148bf650f879fd32e3598bf1b5a1f6a70baf68
losc06001 <losc06001@E-PW0234JE>
2023-09-15 12:26:03 +0200

    Add project files.

---
+++ Uppgift2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+ namespace Uppgift2._1 
+{
+ class Program 
+    {
+     static void Main(string[] args) 
+        {
+            Console.WriteLine("Vilken är din favoritbok?");
+            string x = Console.ReadLine();
+            Console.WriteLine("Din favorit bok är " + x);
+            Console.ReadKey();
+        }
+    }
+}
